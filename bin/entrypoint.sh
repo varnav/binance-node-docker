f97f842d855f69c4f74ba55783ad7d7d05bb425a
Evgeny Varnavskiy <evarnavsky@bytewerk.com>
2019-04-26 18:51:49 -0400

    Add CLI to path

--- bin/entrypoint.sh
+++ bin/entrypoint.sh
@@ -3,4 +3,7 @@
 setup.sh
 set -ex
 chmod +x node-binary/fullnode/${BNET}/${BVER}/linux/bnbchaind
+chmod +x node-binary/cli/${BNET}/${BVER}/linux/bnbcli
+PATH=$PATH:/node-binary/cli/${BNET}/${BVER}/linux/
+export PATH
 exec node-binary/fullnode/${BNET}/${BVER}/linux/bnbchaind start --home ${BNCHOME} --pruning breathe
