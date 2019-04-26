7529dd926422667735abac47f13c9613091eddd4
Evgeny Varnavskiy <evarnavsky@bytewerk.com>
2019-04-26 19:37:48 -0400

    Minor syntax fix

--- bin/entrypoint.sh
+++ bin/entrypoint.sh
@@ -4,6 +4,6 @@ setup.sh
 set -ex
 chmod +x node-binary/fullnode/${BNET}/${BVER}/linux/bnbchaind
 chmod +x node-binary/cli/${BNET}/${BVER}/linux/bnbcli
-PATH=$PATH:/node-binary/cli/${BNET}/${BVER}/linux/
+PATH=${PATH}:/node-binary/cli/${BNET}/${BVER}/linux/
 export PATH
 exec node-binary/fullnode/${BNET}/${BVER}/linux/bnbchaind start --home ${BNCHOME} --pruning breathe
