4a6ab108de89515792b72105a0817d510298d0df
Evgeny Varnavskiy <evarnavsky@bytewerk.com>
2019-04-28 10:50:21 -0400

    Fix test cli copying

--- bin/setup.sh
+++ bin/setup.sh
@@ -7,9 +7,11 @@ fi
 
 cp node-binary/fullnode/${BNET}/${BVER}/linux/bnbchaind /usr/local/bin/
 cp node-binary/cli/${BNET}/${BVER}/linux/bnbcli /usr/local/bin/
+cp node-binary/cli/${BNET}/${BVER}/linux/tbnbcli /usr/local/bin/
 
 chmod +x /usr/local/bin/bnbchaind
 chmod +x /usr/local/bin/bnbcli
+chmod +x /usr/local/bin/tbnbcli
 
 # Turn on console logging
 
