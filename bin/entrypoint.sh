354cc5790b803c147945083df9697e610a17908f
Evgeny Varnavskiy <evarnavsky@bytewerk.com>
2019-04-28 18:19:51 -0400

    sudo -> su

--- bin/entrypoint.sh
+++ bin/entrypoint.sh
@@ -3,4 +3,4 @@
 source setup.sh
 echo "Running $0 as $USER in $PWD"
 set -ex
-exec sudo -u bnbchaind "/usr/local/bin/bnbchaind start --home ${BNCHOME} --pruning breathe"
+su - bnbchaind -c "/usr/local/bin/bnbchaind start --home ${BNCHOME} --pruning breathe"
