12c9c4e422e1c0bc594a05aeff77f52555c7a435
Evgeny Varnavskiy <evarnavsky@bytewerk.com>
2019-04-28 22:06:53 -0400

    Entrypoint tweaks

--- bin/entrypoint.sh
+++ bin/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 source setup.sh
-echo "Running $0 as $LOGNAME in $PWD"
-set -ex
+echo "Running $0 in $PWD"
+set -ev
 su - bnbchaind -c "/usr/local/bin/bnbchaind start --home ${BNCHOME} --pruning breathe"
